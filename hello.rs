ebdb1cbaddedac240e8d9378c99c61f74c13ee23
Dennis Heckman <denheck@gmail.com>
2014-07-30 21:12:59 -0500

    parallel hello world

--- hello.rs
+++ hello.rs
@@ -1,4 +1,13 @@
 fn main() {
+    let greeting_message = "Hello?";
+
     // println! is a macro
-    println!("hello?");
+    println!("{}", greeting_message);
+
+    // parallel hello world
+    for _ in range(0u, 10) {
+        spawn(proc() {
+            println!("{}", greeting_message);
+        });
+    }
 }
